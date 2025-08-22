a8569111fafd2e04940c582570650caefb599709
d-manucharyan <davit.manucharian@gmail.com>
2025-08-22 21:53:19 +0400

    adding coments

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ app.use(express.static('view'))
 app.use(express.urlencoded())
 
 
+// homepage
 app.get('/', (req, res) => {
     res.render(path.join(__dirname, 'view', 'home.ejs'))
 })
 
+// main page 
 app.get('/tasks', readTasks, (req, res) => {
-    const { tasks } = res.locals
-    res.render(path.join(__dirname, 'view', 'tasks.ejs'), { tasks })
+    res.render(path.join(__dirname, 'view', 'tasks.ejs'))
 })
 
+// middleware-ov kardum enq task jsony u avelacnum tasker
 app.post('/tasks', readTasks, async (req, res) => {
     const { tasks } = res.locals
     const task = {
@@ -29,7 +31,7 @@ app.post('/tasks', readTasks, async (req, res) => {
     await fs.unlink(path.join(__dirname, 'task', 'tasks.json'))
     await fs.appendFile(path.join(__dirname, 'task', 'tasks.json'), JSON.stringify(tasks, null, 2))
     res.redirect('/tasks')
-    console.log(tasks)
+    console.log(tasks)  
 })
 
 app.listen(3000)
